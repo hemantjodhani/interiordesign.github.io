6f838188d1b1408733bb403f62798f26c68e28f5
hemantjodhani <131151447+hemantjodhani@users.noreply.github.com>
2023-04-20 02:17:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $('.header-right img').click( function(){
+        $('.search-system').fadeIn();
+    });
+
+    $('.inputs span').click(function(){
+        $('.search-system').fadeOut();
+    });
+
+    $('.inputs input').click(function(){
+        $('.search-system').fadeIn();
+    }); 
+
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+          $('.search-system').fadeOut();
+        }
+    }); 
+
+    $(".search-system .inputs").click( function(e) {
+        // console.log(e);
+        if (this !== e.target) return;
+
+        $(".search-system").fadeOut();
+    } );
+
+});
+
