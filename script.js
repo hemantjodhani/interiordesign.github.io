5c4de068105ebfb0558d1e269791e3e879b881b9
hemantjodhani <jodhani.hemant@gmail.com>
2023-04-20 14:22:55 +0530

    initial

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    $('.header-right img').click( function(){
+        $('.search-system').fadeIn();
+    });
+
+    $('.inputs span').click(function(){
+        $('.search-system').fadeOut();
+    });
+
+    $('.inputs input').click(function(){
+        $('.search-system').fadeIn();
+    }); 
+
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+          $('.search-system').fadeOut();
+        }
+    }); 
+
+    $(".search-system .inputs").click( function(e) {
+        // console.log(e);
+        if (this !== e.target) return;
+
+        $(".search-system").fadeOut();
+    });
+
+    $('a').click(function(e) {
+        $('a').click(function(e) {
+            var height = $(this).attr('class');
+            // alert(myClass)
+            e.preventDefault();
+            $(document).scrollTop(height);
+        });
+    });       
+});
